6a30ebf4ecd48582f5de539e070b344e6ccdb0a9
Farhaan <60516398+mdfarhaan@users.noreply.github.com>
2021-10-11 22:50:38 +0530

    Update cryptoPrice.py

--- Python/cryptoPrice.py
+++ Python/cryptoPrice.py
@@ -3,7 +3,8 @@ import requests
 url = "https://api.wazirx.com/api/v2/tickers" #API 
 response = (requests.get(url).json()) #API Response
 
-def price(coin):
+#Takes in the coin symbol as the parameter
+def price(coin): 
     return response[coin.lower() + "inr"]["last"]
 
 print(price("DOGE"))
